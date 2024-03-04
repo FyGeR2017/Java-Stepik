51e7eeb8d8c513288b2261e4df2f34a3b0ccb70f
FyGeR2017 <91911137+FyGeR2017@users.noreply.github.com>
2024-03-04 15:35:53 +0500

    update

--- task2/src/main/java/servlets/SingUpServlet.java
+++ task2/src/main/java/servlets/SingUpServlet.java
@@ -10,20 +10,12 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class SingUpServlet extends HttpServlet {
-    @SuppressWarnings({"FieldCanBeLocal", "UnusedDeclaration"}) //todo: remove after module 2 home work
     private final AccountService accountService;
 
     public SingUpServlet(AccountService accountService) {
         this.accountService = accountService;
     }
 
-    //get public user profile
-    public void doGet(HttpServletRequest request,
-                      HttpServletResponse response) throws ServletException, IOException {
-        //todo: module 2 home work
-    }
-
-    //sign up
     public void doPost(HttpServletRequest request,
                        HttpServletResponse response) throws ServletException, IOException {
         String login = request.getParameter("+login");
@@ -39,15 +31,4 @@ public class SingUpServlet extends HttpServlet {
         accountService.addNewUser(user);
     }
 
-    //change profile
-    public void doPut(HttpServletRequest request,
-                      HttpServletResponse response) throws ServletException, IOException {
-        //todo: module 2 home work
-    }
-
-    //unregister
-    public void doDelete(HttpServletRequest request,
-                         HttpServletResponse response) throws ServletException, IOException {
-        //todo: module 2 home work
-    }
 }
